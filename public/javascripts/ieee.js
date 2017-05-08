f45466009840d0c5612f94434481d38083549ecd
Miguel Lopez <Icemberth.Mike@gmail.com>
2017-05-07 23:27:48 -0500

    verificación usuario existe o no

--- public/javascripts/ieee.js
+++ public/javascripts/ieee.js
@@ -1,7 +1,8 @@
 var ieee = {};
 ieee.core = {};
-ieee.core.index = {};
 ieee.facebook = {};
+ieee.helpers = {};
+
 
 ieee = {
     init: function(){// FUNCTION INIT
@@ -14,9 +15,38 @@ ieee = {
                 });
             }
         });
-    }//END INIT
+    },//END INIT
+    helpers: {
+      response : "test"
+    }
 }
+ieee.core = {
+  redirect : function(URL){
+    document.location = URL;
+  },
+  post : function(path, params, method) {
+      method = method || "post"; // Set method to post by default if not specified.
 
+      // The rest of this code assumes you are not using a library.
+      // It can be made less wordy if you use one.
+      var form = document.createElement("form");
+      form.setAttribute("method", method);
+      form.setAttribute("action", path);
+
+      for(var key in params) {
+          if(params.hasOwnProperty(key)) {
+              var hiddenField = document.createElement("input");
+              hiddenField.setAttribute("type", "hidden");
+              hiddenField.setAttribute("name", key);
+              hiddenField.setAttribute("value", params[key]);
+
+              form.appendChild(hiddenField);
+           }
+      }
+      document.body.appendChild(form);
+      form.submit();
+  }
+}
 ieee.facebook = {
     init : function(){
         hello.init({
@@ -34,10 +64,19 @@ ieee.facebook = {
     login : function(){
         hello('facebook').login({scope: 'public_profile, email, friends'}).then(function() {
             //return console.log('You are signed in to Facebook');
-            document.location = "/home" ;
+            //document.location = "/home" ;
         }, function(e) {
             console.log('Signin error: ',e.error.message);
         });
 
+    },
+    me : function(callback){
+        hello('facebook').api('me').then(function(fbJson) {
+          ieee.helpers.response = fbJson;
+          callback(fbJson);
+        }, function(e) {
+          ieee.helpers.response = e.error.message;
+          callback(e.error.message);
+        });
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,25 @@
 var express = require('express');
-var getUser = require('../modules/getUser.js');
+var UserModel  = require('../modules/getUser.js');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/user', function(req, res, next) {
+});
 router.get('/user/:id', function(req, res, next) {
-  console.log(req.params.id);
-   var currentUser = getUser("1000", function(other, rows){
-      console.log("Información a listar : ", rows);
-   });
-   /*console.log(typeof currentUser);
-   currentUser = JSON.stringify(currentUser);
-   console.log(typeof currentUser);*/
-   res.json({ user: currentUser});
+
+   UserModel.getUser(req.params.id,function(error, rows){
+    res.json({ user: rows});
+  });
+
+});
+
+router.post('/register/:id', function(req, res, next) {
+  var data = req.body.user;
+  var aux = JSON.parse(data);
+  for(item in aux){
+    console.log(item + " : " + aux[item]);
+  }
+  res.render('register', aux);
 });
 
 module.exports = router;
